25a062db2373f14afc86fd17bef4cfc9f0da075a
hajakuja <uqabra@gmail.com>
2018-01-02 13:57:19 +0100

    initial commit

---
+++ app/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from app.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired() ])
+    password2 = PasswordField('Enter Password Again',\
+                               validators=[DataRequired(), EqualTo('password') ])
+    submit = SubmitField('Register')
+
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Perdoruesi', validators=[DataRequired()])
+    password = PasswordField('Pasi', validators=[DataRequired()])
+    remember_me = BooleanField('Mos me harro')
+    submit = SubmitField('Kyçu')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username: ', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
